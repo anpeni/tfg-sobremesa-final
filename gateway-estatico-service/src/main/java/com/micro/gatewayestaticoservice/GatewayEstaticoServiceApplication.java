c0cf2973900fb5cd82788d840118aca84fcf0ffe
AntonioPerez <p-aperez@neoris.com>
2023-05-22 14:50:44 +0200

    eureka jar1

---
+++ gateway-estatico-service/src/main/java/com/micro/gatewayestaticoservice/GatewayEstaticoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.gatewayestaticoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEstaticoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEstaticoServiceApplication.class, args);
+	}
+
+}
---
+++ gateway-estatico-service/src/test/java/com/micro/gatewayestaticoservice/GatewayEstaticoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.gatewayestaticoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEstaticoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
